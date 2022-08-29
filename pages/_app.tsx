a46eaba76be331a097c70cfa50faf02d8a3cb156
Agailh <agailham86@gmail.com>
2022-08-29 11:12:52 +0700

    navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import Navigation from "../pages/components/navigation";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Navigation />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
